ced99451ac02fd5949fca609495e83002eee8bae
TatyanaKorzhova <96631871+TatyanaKorzhova@users.noreply.github.com>
2022-01-06 22:18:13 +0300

    Group creation tests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,68 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class GroupCreationTests {
+  private WebDriver wb;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wb = new FirefoxDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    wb.get("http://localhost/addressbook/group.php");
+    wb.findElement(By.name("user")).click();
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys("admin");
+    wb.findElement(By.name("pass")).clear();
+    wb.findElement(By.name("pass")).sendKeys("secret");
+    wb.findElement(By.xpath("//input[@value='Login']")).click();
+    wb.findElement(By.linkText("groups")).click();
+    wb.findElement(By.name("new")).click();
+    wb.findElement(By.name("group_name")).click();
+    wb.findElement(By.name("group_name")).clear();
+    wb.findElement(By.name("group_name")).sendKeys("test1");
+    wb.findElement(By.name("group_header")).click();
+    wb.findElement(By.name("group_header")).clear();
+    wb.findElement(By.name("group_header")).sendKeys("test2");
+    wb.findElement(By.name("group_footer")).click();
+    wb.findElement(By.name("group_footer")).clear();
+    wb.findElement(By.name("group_footer")).sendKeys("test3");
+    wb.findElement(By.name("submit")).click();
+    wb.findElement(By.linkText("groups")).click();
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wb.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wb.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wb.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,4 +1,4 @@
-class MyFirstProgram {
+public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
